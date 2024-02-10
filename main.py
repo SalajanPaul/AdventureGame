76681d27dc80e28b00ffcae836236311ea0b56b3
Salajan Paul-Sebastian <salajanpaul1@gmail.com>
2024-02-10 22:19:50 +0200

    v2

--- main.py
+++ main.py
@@ -2,43 +2,51 @@ import random
 
 
 def AdventureGame():
-    player_health = 100
-    wolf_health = 100
-    wolf_damage = 20
-    bear_health = 100
-    bear_damage = 25
-    dragon_boss_health = 100
-    dragon_boss_damage = 45
+    bonus_options = ["hp", "damage"]
+    bonus_hp = 25
+    bonus_damage = 50
+    bonus = random.choices(bonus_options)
+
+    player_health = 250
+    enemies = [
+        {"name": "wolf", "health": 100, "damage": 20},
+        {"name": "bear", "health": 100, "damage": 25},
+        {"name": "dragon", "health": 200, "damage": 30}
+    ]
 
     character_name = input("What is your character name? ")
 
-    sword_damage = 25
-    axe_damage = 30
-    mace_damage = 45
-
     character_weapon = input("What weapon will you like to pick: sword, axe or mace ")
 
-    print(f" ////// Hello {character_name}, your weapon will be {character_weapon}! //////")
+    weapon_damage = {"sword": 25, "axe": 30, "mace": 45}.get(character_weapon, 25)
 
-    while player_health > 0:
-        # first enemy wolf #
-        if character_weapon == "sword":
-            player_health -= wolf_damage
-            print("Your health:", player_health)
-            wolf_health -= sword_damage
-            if wolf_health <= 0:
-                print(f"You won! Remaining health: {player_health}")
-                # second enemy bear #
-                player_health -= bear_damage
-                print("Your health:", player_health)
-                bear_health -= sword_damage
-            if player_health <= 0:
-                print("You lost")
-                break
+    print(f" ////// Hello {character_name}, your weapon will be a {character_weapon}! //////")
+
+    for enemy in enemies:
+        enemy_name = enemy["name"]
+        enemy_health = enemy["health"]
+
+        print(f"You encountered a {enemy_name}")
+
+        while player_health > 0 and enemy_health > 0:
+            # Player's turn
+            enemy_health -= weapon_damage
+            print(f"{enemy_name} health: {enemy_health}")
+
+            # Enemy's turn
+            player_health -= enemy["damage"]
+            print(f"Your health remaining: {player_health}")
+
+
+        if player_health <= 0:
+            print("You died!")
+            break
+
+        print(f"You defeated {enemy_name}!")
+
+    print("Congratulation, you won!")
+
 
 
 
 AdventureGame()
-
-
-
